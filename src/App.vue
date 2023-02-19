c58247eca024142520b3ab75d4d9ab038b4bf2ed
Xander Lubbe <xanderl@bbd.co.za>
2023-02-19 20:43:57 +0200

    Main page updated.

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,70 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import { useToDoListStore } from "./stores/ToDoListStore";
+
+const toDoListStore = useToDoListStore();
+
+onMounted(() => {
+  toDoListStore.fetchData()
+})
+
 </script>
 
 <template>
   <div class="appVue">
     <header>
-      <img alt="Check logo" class="logo" src="@/assets/check.png" width="125" height="125" />
-      <div class="navParent">
+      <img alt="Check logo" class="logo" src="@/assets/done.svg" width="125" height="125" />
         <nav>
-          <RouterLink to="/todo">ToDo</RouterLink>
+          <RouterLink to="/active">ToDo</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav>
-      </div>
-
     </header>
-    <RouterView />
-
-  </div>
+    <main class="mainApp">
+      <RouterView name="nav" />
+      <RouterView />
+    </main>
+</div>
 </template>
 
 <style scoped>
-.appVue {
-  width: 100vw;
+.mainApp {
+  display: flex;
+  flex-direction: column;
   background-color: white;
+  color: black;
+  padding: 20px;
+  font-size: large;
 }
 
 header {
   line-height: 1.5;
-  max-height: 100vh;
   width: 100%;
-  min-width: 1000px;
-  background-color: #007090;
+  background-color: #FEFEFE;
+  position: relative;
+  box-shadow: 0 0px 4px 0px #00000050;
+  z-index: 1;
   display: flex;
   align-items: center;
   flex-wrap: nowrap;
   flex-direction: row;
 }
 
-.navParent {
-  position: relative;
-  height: 75px;
-}
-
 .logo {
   display: block;
-  margin: 0 auto 2rem;
   text-align: left;
-  padding: 10px;
 }
 
 nav {
-  width: auto;
-  font-size: 12px;
-  text-align: left;
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-  display: flex;
+  font-size: 16px;
 }
 
 nav a.router-link-exact-active {
   color: var(--color-text);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-  background-color: #EAEBED;
-}
-
-
-
-@media (min-width: 1024px) {
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-    padding: 1rem 0;
-  }
-
 }
 </style>
