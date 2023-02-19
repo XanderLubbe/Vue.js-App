1b5eb7a10fa7533e4740c0832e92dfbb81a2e934
Xander Lubbe <xanderl@bbd.co.za>
2023-02-19 20:43:20 +0200

    Stores and routes updated.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,50 @@
 import AboutVue from "@/views/About.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import ToDo from "../views/ToDo.vue";
+import ActiveToDoBox from "@/components/ActiveToDoBox.vue"
+import ArchivedToDoBox from "@/components/ArchivedToDoBox.vue"
+import DeletedToDoBox from "@/components/DeletedToDoBox.vue"
+import NavBar from "@/components/NavBar.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/todo",
-      name: "todo",
-      component: ToDo,
+      path: "/",
+      components: {
+        default: ActiveToDoBox,
+        nav: NavBar,
+      }
     },
     {
       path: "/about",
       name: "about",
       component: AboutVue,
     },
+    {
+      path: "/active",
+      name: "active",
+      components: {
+        default: ActiveToDoBox,
+        nav: NavBar,
+      }
+    },
+    {
+      path: "/archive",
+      name: "archive",
+      components: {
+        default: ArchivedToDoBox,
+        nav: NavBar,
+      }
+    },
+    {
+      path: "/deleted",
+      name: "deleted",
+      components: {
+        default: DeletedToDoBox,
+        nav: NavBar,
+      }
+    },
+
 
   ],
 });
--- src/stores/ToDoListStore.ts
+++ src/stores/ToDoListStore.ts
@@ -1,5 +1,6 @@
+import { Actions, type Undo } from '@/UndoType';
 import { defineStore } from 'pinia';
-import type { ToDo } from "../ToDoType"
+import { ItemState, type ToDo } from "../ToDoType"
 
 export const useToDoListStore = defineStore("ToDoListStore", {
 
@@ -8,27 +9,26 @@ export const useToDoListStore = defineStore("ToDoListStore", {
         datahubError: "",
         piniaErrorState: false,
         piniaDisplayValue: false,
-        undoList: [] as ToDo[],
+        undoList: [] as Undo[],
     }),
     actions: {
         fetchData() {
-            console.log("fetching things")
 
-            fetch('http://10.103.184.35:9200/data/todo-app/get-todos.json')
+            fetch('http://192.168.68.101:9200/data/todo/todo.json')
                 .then((response) =>
                     response.json(),
                 )
                 .then((data) => {
-
-                    if (data.resultCode > 299) {
+                    // fix
+                    if (data.resultCode == "500") {
                         this.datahubError = data.message
                         this.piniaErrorState = true
                         return data.resultCode
                     }
-
+                    //todo - list is more right
                     for (let i = 0; i < data.Todos.length; i++) {
                         this.piniaErrorState = false
-                        this.itemList[i] = { id: data.Todos[i].id, text: data.Todos[i].text, action: "add", textTreatment: undefined }
+                        this.itemList[i] = { id: data.Todos[i].id, text: data.Todos[i].text, itemState: ItemState.Active, isCompleted: false }
                         this.datahubError = ""
                     }
                 }).catch(error => {
@@ -37,42 +37,64 @@ export const useToDoListStore = defineStore("ToDoListStore", {
                     return 500
                 })
         },
+
+        // CRUD
         addItem(item: ToDo) {
             this.itemList.push(item)
         },
-        removeItem(id: number) {
+        permanentlyRemoveItem(id: number) {
             const i = this.itemList.findIndex((item) => {
                 return item.id == id
             })
 
             if (i > -1) this.itemList.splice(i, 1)
         },
+        editItem(item: ToDo) {
+            const index = this.findIndexOfSelectedItem(item.id)
+            this.itemList[index].text = item.text
+            this.itemList[index].itemState = item.itemState
+            this.itemList[index].isCompleted = item.isCompleted
+        },
+        addUndoItem(item: Undo) {
+            this.undoList.push(item)
+        },
+        determineUndoAction(item: Undo) {
+            switch (item.action) {
+                case Actions.Add:
+                    this.permanentlyRemoveItem(item.ToDo.id);
+                    break;
+                case Actions.Edit:
+                case Actions.Remove:
+                    this.editItem(item.ToDo)
+                    break;
+            }
+        },
+
+        // item state modifiers
+        setToActiveItem(item: ToDo) {
+            this.changeItemState(item, Actions.Add, ItemState.Active)
+        },
+        setToArchiveItem(item: ToDo) {
+            this.changeItemState(item, Actions.Edit, ItemState.Archived)
+        },
+        setToDeletedItem(item: ToDo) {
+            this.changeItemState(item, Actions.Remove, ItemState.Deleted)
+        },
+
+        // helper functions
         findIndexOfSelectedItem(id: number) {
             const i = this.itemList.findIndex((item) => {
                 return item.id == id
             })
             return i
         },
-        selectedItem(id: number) {
-            return 0
+        changeItemState(item: ToDo, action: Actions, itemState: ItemState) {
+            const newUndoItem: Undo = { action, ToDo: { ...item } }
+            this.addUndoItem(newUndoItem)
+            item.itemState = itemState
         },
-        editItem(id: number, text: string) {
-            const index = this.findIndexOfSelectedItem(id)
-            this.itemList[index].text = text;
-            this.itemList[index].action = "edit"
-        },
-        addUndoItem(item: ToDo) {
-            this.undoList.push(item)
-        },
-        holdUndoValues(item: ToDo) {
-            if (item.action == "add") {
-                this.removeItem(item.id)
-            } else if (item.action == "edit") {
-                // FIXME: ID is not an index!
-                this.editItem(item.id, item.text)
-            } else if (item.action == "remove") {
-                this.addItem(item)
-            }
+        showAll() {
+            console.log("Items in your store", this.itemList)
         }
     }
 });
